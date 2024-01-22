6cd21ec70f1c1f8695736725faa5c0af9f7339db
rakib13x <rakib13x@gmail.com>
2024-01-23 04:51:03 +0600

    firebase added

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAqbcATXlqhUKCiLXt4nwmf2vIkbax1a9o",
+  authDomain: "house-renter-86cc2.firebaseapp.com",
+  projectId: "house-renter-86cc2",
+  storageBucket: "house-renter-86cc2.appspot.com",
+  messagingSenderId: "626020452918",
+  appId: "1:626020452918:web:be0577ba2c3b33ead3a697",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
